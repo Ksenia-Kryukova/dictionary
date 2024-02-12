89aaf7f92bcf389fdd0871774fd26860beb44735
Ksenia-Kryukova <ksyusha-kryukova@bk.ru>
2024-02-12 21:19:57 +0100

    work_app

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
         path('', views.home, name='home'),
-        path('/home', views.home, name='home'),
-        path('/', views.home, name='home'),
-        path('/words_list', views.words_list, name='words_list'),
-        path('/add_word', views.add_word, name='add_word')
+        path('home', views.home, name='home'),
+        path('words_list', views.words_list, name='words_list'),
+        path('add_word', views.add_word, name='add_word')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render, redirect
 
 
-words = [
-    {
-        'Original': 'original',
-        'Translated': 'translated'
-    }
-]
-
-
 def home(request):
     return render(request, 'home.html', {'page': 'home'})
 
 
 def words_list(request):
-    return render(request, 'word_list.html', {'words': words})
+    with open("/home/kryukova/Dictionary_study/dictionary/my_app/words_list.txt", 'r', encoding='utf-8') as file:
+        content = file.read().splitlines()
+
+    words1 = []
+    words2 = []
+    for line in content:
+        word1, word2 = line.split("-")
+        words1.append(word1)
+        words2.append(word2)
+
+    return render(request, 'words_list.html', {'words1': words1, 'words2': words2})
 
 
 def add_word(request):
     if request.method == 'GET':
         return render(request, "add_word.html", {"page": "add_word"})
+
     else:
         print(request.POST)
         with open("/home/kryukova/Dictionary_study/dictionary/my_app/words_list.txt", "a") as file:
-            file.writelines(f"word1: {request.POST['word1']}, word2: {request.POST['word2']}")
-        return redirect(words_list)
+            file.write(request.POST['word1'] + "-" + request.POST['word2'] + "\n")
+
+        return redirect(home)
